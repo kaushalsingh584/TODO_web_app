fe24fe7344ce6332e002535cc6514226177032fc
kaushalsingh585 <kaushalsingh584@gmail.com>
2023-01-23 11:00:27 +0530

    added files for deploing

--- TODO_web_app/TODO_web_app/settings.py
+++ TODO_web_app/TODO_web_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_6ej)x7h9r+7l96+yoyiaq93hsavnouv8hmb=8vk3$xae%4vwy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
